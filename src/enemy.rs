db287735702642521c74cb183801a7f568e525d8
TyLanger <ty.langer16@gmail.com>
2022-09-20 22:47:56 -0600

    Spawning Enemies
    Moved the spawn enemy code so it can be reused
    Make an event to have an enemy spawn
    
    fn spawn_enemies makes 5 events to spawn 5 enemies
    fn when_to_spawn_enemies makes an event if there are ever <2 enemies
    This also causes 1 extra enemy to be spawned at the start
    
    fn how_to_spawn_enemies
    moved the code from spawn_enemies to here
    Just pick a random spot around a circle centered on the origin (center of
    screen)
    
    Are these naming conventions (how_to, when_to) more clear?

--- src/enemy.rs
+++ src/enemy.rs
@@ -12,14 +12,60 @@ pub struct EnemyPlugin;
 impl Plugin for EnemyPlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system(spawn_enemies)
+            .add_event::<EnemySpawnEvent>()
+            .add_system(how_to_spawn_enemies)
+            .add_system(when_to_spawn_enemies)
             .add_system(enemy_movement);
     }
 }
 
-fn spawn_enemies(mut commands: Commands) {
+struct EnemySpawnEvent;
+
+fn spawn_enemies(mut ev_spawn: EventWriter<EnemySpawnEvent>) {
     let num = 5;
 
+    // spawn 5 enemies at the start.
+    // when_to_spawn will also queue an enemy to spawn
+    // as there are 0 at startup
+    // and this event is run next frame
+    // giving when_to 1 frame at the start to see 0 enemies
     for _ in 0..num {
+        ev_spawn.send(EnemySpawnEvent);
+    }
+}
+
+fn enemy_movement(
+    q_player: Query<&Transform, (With<Player>, Without<Enemy>)>,
+    mut q_enemy: Query<&mut Transform, With<Enemy>>,
+    time: Res<Time>,
+) {
+    let player_pos = q_player.get_single().unwrap().translation;
+
+    for mut enemy_trans in q_enemy.iter_mut() {
+        let dir = player_pos - enemy_trans.translation;
+        enemy_trans.translation += dir.normalize_or_zero() * 100. * time.delta_seconds();
+    }
+}
+
+fn when_to_spawn_enemies(mut ev_spawn: EventWriter<EnemySpawnEvent>, q_enemies: Query<&Enemy>) {
+    // check how many enemies there are
+    let num_enemies = q_enemies.iter().len();
+
+    // each frame, check if there are fewer than 2 enemies.
+    // if there is, spawn one enemy
+    // enemy will be spawned next frame
+    // will always spawn 1 enemy on startup
+    // as this sees 0 enemies before they have a chance to spawn
+    if num_enemies < 2 {
+        ev_spawn.send(EnemySpawnEvent);
+    }
+}
+
+fn how_to_spawn_enemies(mut commands: Commands, mut ev_spawn: EventReader<EnemySpawnEvent>) {
+    // send an event to get this to spawn an enemy
+
+    // spawn an enemy for each event
+    for _ in ev_spawn.iter() {
         let mut rng = rand::thread_rng();
         let spawn_pos = Vec2::new(rng.gen_range(-1.0..=1.0), rng.gen_range(-1.0..=1.0))
             .normalize_or_zero()
@@ -40,18 +86,5 @@ fn spawn_enemies(mut commands: Commands) {
             .insert(RigidBody::Dynamic)
             .insert(LockedAxes::ROTATION_LOCKED)
             .insert(Collider::cuboid(35.0 / 2.0, 35.0 / 2.0));
-    }
-}
-
-fn enemy_movement(
-    q_player: Query<&Transform, (With<Player>, Without<Enemy>)>,
-    mut q_enemy: Query<&mut Transform, With<Enemy>>,
-    time: Res<Time>,
-) {
-    let player_pos = q_player.get_single().unwrap().translation;
-
-    for mut enemy_trans in q_enemy.iter_mut() {
-        let dir = player_pos - enemy_trans.translation;
-        enemy_trans.translation += dir.normalize_or_zero() * 100. * time.delta_seconds();
     }
 }
